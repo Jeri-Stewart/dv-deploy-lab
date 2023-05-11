26d217315ed3a57b83e47f4002b5517c03660cf9
Jeri Stewart <jerijuba@gmail.com>
2023-05-11 16:35:28 -0400

    3rd commit

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
-const express = require('express')
-const app = express()
-app.listen(4000, () => console.log(`server running on 4000`))
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path")
 
+const app = express();
 
-app.use(express.static(`${__dirname}/public`))
+app.listen(4000, () => console.log(`server running on 4000`));
+
+app.use(express.static(`${__dirname}/public`));
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+const {ROLLBAR_ACCESS_TOKEN} = process.env;
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+// create an endpoint
+
+try {
+    app.get('/', fakeFuncion);
+  } catch (error) {
+    rollbar.error(error);
+   
+  }
